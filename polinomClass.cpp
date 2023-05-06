d17d2a97ece09a36060500e21cea6fa7fbf81c38
Mehmet Dinçer <dmehmett38@gmail.com>
2023-05-06 13:58:03 +0300

    Create polinomClass.cpp

---
+++ polinomClass.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Employee {
+private:
+	int salary;
+};
+
+class Programmer : Employee{
+public:
+	int bonus;
+	void setSalary(int s)
+	{
+		salary = s;
+	}
+	
+	int getSalary()
+	{
+		return salary;
+	}
+};
+
+1234	Faruk	22250
+1234	Faruk	22250
+
+Int: 8
+Double: 13.743
